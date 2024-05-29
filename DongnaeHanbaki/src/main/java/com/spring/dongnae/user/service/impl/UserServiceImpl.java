f64c3f5af2f747a419e5e3d9478e327ff3cb256e
ITWILL <ITWILL@DESKTOP-6A2MMH4>
2024-05-29 16:45:06 +0900

    0529_project_structure

--- DongnaeHanbaki/src/main/java/com/spring/dongnae/user/service/impl/UserServiceImpl.java
+++ DongnaeHanbaki/src/main/java/com/spring/dongnae/user/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.spring.dongnae.service.user.impl;
+package com.spring.dongnae.user.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
