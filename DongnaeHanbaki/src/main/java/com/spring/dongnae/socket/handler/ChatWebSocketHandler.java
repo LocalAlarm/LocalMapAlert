fd95396e2cd7a4e1f9e544f53ca3a99542a6b1bd
RTPC <hyhy9501@naver.com>
2024-06-09 15:56:54 +0900

    get meaage from repository

--- DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/handler/ChatWebSocketHandler.java
+++ DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/handler/ChatWebSocketHandler.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -17,7 +15,6 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import com.spring.dongnae.socket.scheme.ChatMessage;
 import com.spring.dongnae.socket.scheme.ChatMessageRepository;
 import com.spring.dongnae.user.service.UserService;
-import com.spring.dongnae.user.vo.CustomUserDetails;
 
 @Component
 public class ChatWebSocketHandler extends TextWebSocketHandler {
@@ -40,9 +37,22 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     	String token = (String) session.getAttributes().get("userToken");
     	 if (token != null) {
              sessions.put(session, token);
-             System.out.println("Connected: " + token);
+             // 사용자 ID로 기존 메시지 문서 검색
+             Optional<ChatMessage> optionalChatMessage = chatMessageRepository.findByUserId(token);
+             ChatMessage chatMessage;
+             // 기존 채팅방이 있으면 해당 데이터를 가져옴
+             if (optionalChatMessage.isPresent()) {
+                 chatMessage = optionalChatMessage.get();
+                 for (Map<String, String> messageMap : chatMessage.getMessages()) {
+                 	String fromToken = messageMap.get("from");
+                 	String dbMessage = messageMap.get("message");
+                 	session.sendMessage(new TextMessage(userService.getUserByToken(fromToken).getNickname() + ": " + dbMessage));
+                 }
+             } else {
+                 // 새로운 메시지 문서 생성
+                 chatMessage = new ChatMessage(token);
+             }
          } else {
-             System.out.println("No authenticated user found.");
              session.close(CloseStatus.NOT_ACCEPTABLE);
          }
     }
@@ -51,26 +61,17 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String token = sessions.get(session);
         String payload = message.getPayload();
-        
-
-        // 사용자 ID로 기존 메시지 문서 검색
         Optional<ChatMessage> optionalChatMessage = chatMessageRepository.findByUserId(token);
-        ChatMessage chatMessage;
-
-        if (optionalChatMessage.isPresent()) {
-            // 기존 메시지 문서에 새로운 메시지 추가
-            chatMessage = optionalChatMessage.get();
-        } else {
-            // 새로운 메시지 문서 생성
-            chatMessage = new ChatMessage(token);
-        }
+        ChatMessage newMessage;
+        newMessage = optionalChatMessage.get();
+        
         
         //현재 로그인한 유저의 토큰값 가져와서 hashmap으로 만들어야함.
         HashMap<String, String> messageMap = new HashMap<String, String>();
         messageMap.put("from", token);
         messageMap.put("message", payload);
-        chatMessage.addMessage(messageMap);
-        chatMessageRepository.save(chatMessage);
+        newMessage.addMessage(messageMap);
+        chatMessageRepository.save(newMessage);
 
         // Broadcast the message to all connected clients
         for (WebSocketSession s : sessions.keySet()) {
