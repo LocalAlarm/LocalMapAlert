8878f8c683adcd3ff234a9f2d736e945d90ff54e
RTPC <hyhy9501@naver.com>
2024-06-09 16:56:21 +0900

    findByUserToken 추후 구현

--- DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/scheme/ChatMessageRepository.java
+++ DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/scheme/ChatMessageRepository.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
     Optional<ChatMessage> findByUserId(String userId);
+    //Optional<ChatMessage> findByToken(String token);
 }
