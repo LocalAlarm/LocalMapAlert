017d60d668ce2b209dc9385d6e0b5c70787505a6
Chung-0904 <164819447+Chung-0904@users.noreply.github.com>
2024-06-14 18:19:49 +0900

    친구요청 승인 까지함 / 거절 작업 전

--- DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/controller/FriendRequestController.java
+++ DongnaeHanbaki/src/main/java/com/spring/dongnae/socket/controller/FriendRequestController.java
@@ -1,6 +1,6 @@
 package com.spring.dongnae.socket.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -12,9 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.dongnae.socket.repo.ChatRoomRepository;
 import com.spring.dongnae.socket.repo.FriendRoomRepository;
+import com.spring.dongnae.socket.scheme.ApproveFriendRequest;
+import com.spring.dongnae.socket.scheme.ChatRoom;
+import com.spring.dongnae.socket.scheme.FriendInfo;
 import com.spring.dongnae.socket.scheme.FriendRequest;
 import com.spring.dongnae.socket.scheme.FriendRoom;
+import com.spring.dongnae.user.service.UserService;
 import com.spring.dongnae.utils.auth.GetAuthenticInfo;
 
 @RestController
@@ -24,10 +29,19 @@ public class FriendRequestController {
     private final GetAuthenticInfo getAuthenticInfo;
     @Autowired
 	private final FriendRoomRepository friendRoomRepository;
+    @Autowired
+    private final UserService userService;
+    @Autowired
+    private final ChatRoomRepository chatRoomRepository;
     
-    public FriendRequestController(FriendRoomRepository friendRoomRepository, GetAuthenticInfo getAuthenticInfo) {
+    private final ApproveFriendRequest approveFriendRequest;
+    
+    public FriendRequestController(FriendRoomRepository friendRoomRepository, GetAuthenticInfo getAuthenticInfo, ApproveFriendRequest approveFriendRequest, UserService userService, ChatRoomRepository chatRoomRepository) {
 		this.getAuthenticInfo = getAuthenticInfo;
 		this.friendRoomRepository = friendRoomRepository;
+		this.userService = userService;
+		this.chatRoomRepository = chatRoomRepository;
+		this.approveFriendRequest = approveFriendRequest;
     }
     
     @PostMapping("/api/sendFriendRequest") // POST 요청을 처리하는 메서드를 정의합니다.
@@ -46,15 +60,72 @@ public class FriendRequestController {
 
     //친구요청 받아온걸 화면에 띄워주는 코드
     @PostMapping("/api/receiveFriendRequest")
-    public ResponseEntity<List<FriendRoom>> receiveFriendRequest(@RequestBody FriendRequest friendRequest) {
-        Optional<FriendRoom> optionalReceiveFriend = friendRoomRepository.findByEmail(friendRequest.getRequestEmail()); // 친구요청 받는 사용자 이메일로 찾기
+    public ResponseEntity<List<String>> receiveFriendRequest() {
+        Optional<FriendRoom> optionalReceiveFriend = friendRoomRepository.findByEmail(getAuthenticInfo.GetEmail()); // 친구요청 받는 사용자 이메일로 찾기
         if (optionalReceiveFriend.isPresent()) { // 있으면
             FriendRoom friendRoom = optionalReceiveFriend.get();
             // 필요한 로직 수행 후 친구 요청 목록 반환 
-            List<FriendRoom> friendRequests = Arrays.asList(friendRoom); 
-            return ResponseEntity.ok(friendRequests);
+            return ResponseEntity.ok(friendRoom.getRequestIds());
         }
         return ResponseEntity.notFound().build();
     }
     
+    @PostMapping(value = "/api/approveFriendRequest", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> approveFriendRequest(@RequestBody ApproveFriendRequest approveRequest) {
+        try {
+            String requestId = approveRequest.getRequestId();
+            Optional<FriendRoom> optionalFriendRoom = friendRoomRepository.findByEmail(requestId);
+            Optional<FriendRoom> optionalMyFriendRoom = friendRoomRepository.findByEmail(getAuthenticInfo.GetEmail());
+            ChatRoom chatRoom = new ChatRoom();
+            
+            List<String> list = new ArrayList<String>();
+            list.add(userService.getUserByEmail(requestId).getToken());
+            list.add(getAuthenticInfo.GetToken());
+            chatRoom.setUserIds(list);
+            chatRoomRepository.save(chatRoom);
+            
+            System.out.println(chatRoom.toString());
+            if (optionalFriendRoom.isPresent()) {
+                FriendRoom friendRoom = optionalFriendRoom.get();
+
+                // 친구 요청 목록에서 해당 요청 제거
+                friendRoom.getRequestIds().remove(getAuthenticInfo.GetEmail());
+                FriendInfo friendInfo = new FriendInfo();
+                System.out.println(requestId);
+                friendInfo.setToken(userService.getUserByEmail(getAuthenticInfo.GetEmail()).getToken());
+                friendInfo.setRoomName(getAuthenticInfo.GetEmail());
+                friendInfo.setRoomId(chatRoom.getId());
+                friendRoom.getFriendIds().add(friendInfo);
+                friendRoomRepository.save(friendRoom);
+                // 친구 목록에 요청한 이메일 추가
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+            
+            if (optionalMyFriendRoom.isPresent()) {
+                FriendRoom friendRoom = optionalMyFriendRoom.get();
+
+                // 친구 요청 목록에서 해당 요청 제거
+                friendRoom.getRequestIds().remove(requestId);
+                FriendInfo friendInfo = new FriendInfo();
+                friendInfo.setToken(userService.getUserByEmail(requestId).getToken());
+                friendInfo.setRoomName(requestId);
+                friendInfo.setRoomId(chatRoom.getId());
+                friendRoom.getFriendIds().add(friendInfo);
+                friendRoomRepository.save(friendRoom);
+                // 친구 목록에 요청한 이메일 추가
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok("친구 요청을 수락하였습니다.");
+        } catch (Exception e) {
+        	e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("친구 요청 수락 중 오류가 발생하였습니다.");
+        }
+    }
+    
+    public ResponseEntity<String> rejectFriendRequest(@RequestBody ApproveFriendRequest approveRequest) {
+		return null;
+    	
+    }
 }
