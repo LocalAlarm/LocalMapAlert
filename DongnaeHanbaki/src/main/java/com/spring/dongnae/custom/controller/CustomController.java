5e241d94410cafefcc48e85016432dd9ba0cfaa0
ITWILL <ITWILL@DESKTOP-6A2MMH4>
2024-06-20 14:42:28 +0900

    0620

--- DongnaeHanbaki/src/main/java/com/spring/dongnae/custom/controller/CustomController.java
+++ DongnaeHanbaki/src/main/java/com/spring/dongnae/custom/controller/CustomController.java
@@ -149,6 +149,8 @@ public class CustomController {
 		//커스텀맵 편집페이지 이동
 		//로그인여부 확인 필요 , false : 로그인 페이지로 이동
 		//mapIdx로 불러온 customMapVO 필요
+		
+//		customService.selectMarker(MapIdx);
 		//커스텀맵에서 사용한 마커종류 리스트 필요
 		//표시한 마커목록 리스트 필요
 		return "map/updateCustMap"; 
